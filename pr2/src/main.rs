ee4ce4717aab99c2c971c330c400e810c77453ff
7oxotsist <lom23200323@gmail.com>
2025-03-13 22:36:19 +0300

    added file stats handling

--- pr2/src/main.rs
+++ pr2/src/main.rs
@@ -3,43 +3,60 @@ extern crate rocket;
 use rocket::form::Form;
 use rocket::fs::TempFile;
 use rocket::http::Status;
+use rocket::tokio::io::AsyncReadExt;
 use std::path::Path;
 use uuid::Uuid;
-use std::fs;
+
 #[derive(FromForm)]
 struct Upload<'f> {
     upload: TempFile<'f>,
 }
 
-// async fn filestats() -> &str {
-    
-// }
+impl Upload<'_> {
+    // Бог меня простил
+    async fn filestats(&self) -> Result<String, Box<dyn std::error::Error>> {
+        let mut buff = String::new();
+        self.upload
+            .open()
+            .await
+            .unwrap()
+            .read_to_string(&mut buff)
+            .await?;
+        let filestats = format!(
+            "Имя файла: {} \nСтрок: {}, Слов: {}, Символов: {}",
+            self.upload.name().unwrap(),
+            buff.lines().count().to_string(),
+            buff.split_whitespace().count().to_string(),
+            buff.chars().count().to_string()
+        );
+        Ok(filestats)
+    }
+}
 
 #[post("/upload", data = "<file>")]
-async fn upload(mut file: Form<Upload<'_>>) -> (Status, &str) {
+async fn upload(mut file: Form<Upload<'_>>) -> (Status, String) {
     if file.upload.content_type().unwrap().is_text() {
         let upload_dir = Path::new("./downloaded");
         println!("file = {:?}", file.upload);
-
+        let stats = file.filestats().await.unwrap();
         let file_id = Uuid::new_v4()
             .hyphenated()
             .encode_lower(&mut Uuid::encode_buffer())
             .to_owned();
         let file_dist = upload_dir.join(&file_id);
 
-        println!("destination = {}", &file_dist.display());
-        println!("length = {} bytes", file.upload.len());
+        // println!("destination = {}", &file_dist.display());
+        // println!("length = {} bytes", file.upload.len());
 
         let _ = file.upload.move_copy_to(&file_dist).await;
 
-        (Status::Ok, "СЮДА ВЫВОДИТЬ")
+        (Status::Ok, stats)
     } else {
-        (Status::NotAcceptable, "Неверный тип файла")
+        (Status::NotAcceptable, "Неверный тип файла".to_owned())
     }
 }
 
 #[launch]
 fn rocket() -> _ {
-    rocket::build()
-        .mount("/", routes![upload])
+    rocket::build().mount("/", routes![upload])
 }

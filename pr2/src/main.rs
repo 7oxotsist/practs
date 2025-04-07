8ef4eb008209914fa888a8b939c20827bff19690
7oxotsist <lom23200323@gmail.com>
2025-04-07 23:26:51 +0300

    added summ and tokio tasks

--- pr2/src/main.rs
+++ pr2/src/main.rs
@@ -1,49 +1,48 @@
-use std::{ffi::OsString, fs::File, io::Read, ptr::null};
 
-use clap::{builder::OsStr, Arg, Command, Error};
+use std::{ffi::OsString, path::Path, sync::Arc};
 
+use clap::{Arg, Command};
+use tokio::fs;
 
 struct Files {
     name: OsString,
-    context: String
+    context: String,
 }
 
 impl Files {
-    fn new(name: OsString, context: String) -> Self {
-        Self { name: name, context: context }
-    }
-
-    async fn file_analyse(&self) -> String {
-        let file_stats = format!(
-            "Имя файла: {}\nСтрок: {}\nСлов: {}\nСимволов: {}\n",
-            self.name.to_str().unwrap(),
+    async fn file_analyse(&self) -> (String, usize, usize, usize) {
+        (
+            self.name.to_str().unwrap().to_string(),
             self.context.lines().count(),
             self.context.split_whitespace().count(),
             self.context.chars().count()
-        );
-        file_stats
+        )
     }
 }
 
 async fn file_handler(dists: Vec<String>) -> Vec<Files> {
-    let mut vec: Vec<Files> = vec![];
-    for dist in dists.iter() {
-        let mut buf: String = "".to_string();
-        let path = std::path::Path::new(dist);
-        let _ = File::open(path).expect("Неверный путь").read_to_string(&mut buf);
-        vec.push(Files{name: path.file_name().unwrap().to_owned(), context: buf});
+    let mut handles = vec![];
+    
+    for dist in dists {
+        handles.push(tokio::spawn(async move {
+            let path = Path::new(&dist);
+            let context = fs::read_to_string(path).await.expect("Неверный путь");
+            Files {
+                name: path.file_name().unwrap().to_owned(),
+                context,
+            }
+        }));
     }
-    vec
-    /* `Vec<File>` value */
+
+    let mut results = vec![];
+    for handle in handles {
+        results.push(handle.await.unwrap());
+    }
+    results
 }
 
 #[tokio::main]
 async fn main() {
-
-    // парсим аргументы -> ищем файлы -> тащим строки и пихаем в вектор объекта
-    // как найти файл? проверка на абсолют пас -> в текущей папке -> при отказе скан 
-    // String в куче -> можно забить и хранить в объекте
-    // 
     let matches = Command::new("FileProcessor")
         .arg(
             Arg::new("files")
@@ -51,17 +50,33 @@ async fn main() {
                 .long("files")
                 .value_name("FILE")
                 .num_args(1..)
-                .required(true)
+                .required(true),
         )
         .get_matches();
 
-    let files = file_handler(matches.get_many::<String>("files")
-        .expect("At least one file required")
-        .cloned()
-        .collect()).await;
-    
-    for (i, file) in files.iter().enumerate() {
-        println!("{}", format!("Файл №{}", i+1));
-        println!("{}", file.file_analyse().await);
+    let files = file_handler(
+        matches
+            .get_many::<String>("files")
+            .expect("At least one file required")
+            .cloned()
+            .collect(),
+    )
+    .await;
+
+    let mut analysis_handles = vec![];
+    for file in files {
+        analysis_handles.push(tokio::spawn(async move {
+            (file.name.clone(),
+             file.file_analyse().await)
+        }));
+    }
+    let mut sum = [0,0];
+    for (i, handle) in analysis_handles.into_iter().enumerate() {
+        let (name, stats) = handle.await.unwrap();
+        println!("Файл №{}: {}", i + 1, name.to_str().unwrap());
+        println!("Имя файла: {}\nСтрок: {}\nСлов: {}\nСимволов: {}\n", stats.0, stats.1, stats.2, stats.3);
+        sum[0] += stats.2;
+        sum[1] += stats.3;
+        println!("Итог: {} слов, {} символов", sum[0], sum[1])
     }
 }
